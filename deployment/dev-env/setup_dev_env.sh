0544e5aada64a18f42c8488698bbc7fa7a8c74ac
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-01-13 06:46:19 -0800

    [Setup] Added a script to setup the dev environment
    The new `setup_dev_env.sh` script installs all required packages,
    a Python virtual environment and the pyaleph module in editable
    mode.

---
+++ deployment/dev-env/setup_dev_env.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+ROOT_DIR="${SCRIPT_DIR}/../.."
+
+cd "${ROOT_DIR}"
+
+sudo apt update && sudo apt install \
+    libbz2-dev \
+    libgflags-dev \
+    libgmp-dev \
+    libleveldb-dev \
+    liblz4-dev \
+    librocksdb-dev \
+    libsecp256k1-dev \
+    libsnappy-dev \
+    libssl-dev \
+    libyaml-dev \
+    pkg-config \
+    zlib1g-dev
+
+if [ ! -d ${ROOT_DIR}/venv ]; then
+    python3 -m virtualenv venv
+fi
+
+source venv/bin/activate
+pip install -e .
+
