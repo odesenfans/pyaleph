36d45325a3f6c2f20590bc116733e44dfdd5b19e
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-01-05 14:20:53 +0100

    Feature: support multiple addresses for balance posts

--- src/aleph/handlers/message_handler.py
+++ src/aleph/handlers/message_handler.py
@@ -66,7 +66,7 @@ class MessageHandler:
         self.content_handlers = {
             MessageType.aggregate: AggregateMessageHandler(),
             MessageType.post: PostMessageHandler(
-                balances_address=config.aleph.balances.address.value,
+                balances_addresses=config.aleph.balances.addresses.value,
                 balances_post_type=config.aleph.balances.post_type.value,
             ),
             MessageType.program: ProgramMessageHandler(),
