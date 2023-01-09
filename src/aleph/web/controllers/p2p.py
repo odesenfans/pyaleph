c99c9e4c500076e337925a967ccb3082c925364f
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-01-09 21:08:25 +0100

    Feature: POST /messages/ endpoint
    Problem: the current way to send messages is:
    1. too permissive, as it allows to publish on any P2P channel
    2. only allows for async posting.
    
    Solution: introduce a new endpoint, POST /messages/.
    
    This endpoint is the new default for the SDKs and applications
    publishing Aleph messages. It supports two modes of publishing:
    sync and async. In async mode, the behaviour is similar to
    /p2p/pubsub/pub/ as the message is validated, shared with
    the other nodes and prepared for processing. The main difference
    is that the endpoint will then return with a 202 instead of a 200
    code.
    
    In sync mode, the endpoint sets up a connection to the internal
    Aleph message MQ and waits for the processing of the message.
    It then returns:
    
    * a 200 if the message was successfully processed
    * a 422 if an error occurred
    * a 202 if the processing of the message took longer than expected.

--- src/aleph/web/controllers/p2p.py
+++ src/aleph/web/controllers/p2p.py
@@ -1,22 +1,43 @@
 import asyncio
 import json
 import logging
-from typing import Dict, cast
+from typing import Dict, cast, Optional, Any, Mapping, List, Collection, Union
 
+import aio_pika.abc
 from aiohttp import web
 from aleph_p2p_client import AlephP2PServiceClient
 from configmanager import Config
+from pydantic import BaseModel, Field, ValidationError
 
-from aleph.schemas.pending_messages import parse_message
+import aleph.toolkit.json as aleph_json
+from aleph.schemas.pending_messages import parse_message, BasePendingMessage
 from aleph.services.ipfs import IpfsService
 from aleph.services.p2p.pubsub import publish as pub_p2p
-from aleph.types.message_status import InvalidMessageException
+from aleph.types.message_status import (
+    InvalidMessageException,
+    MessageStatus,
+    MessageProcessingStatus,
+)
 from aleph.types.protocol import Protocol
+from aleph.web.controllers.app_state_getters import (
+    get_config_from_request,
+    get_ipfs_service_from_request,
+    get_p2p_client_from_request,
+    get_mq_conn_from_request,
+)
+from aleph.web.controllers.utils import mq_make_aleph_message_topic_queue
 
-LOGGER = logging.getLogger("web.controllers.p2p")
+LOGGER = logging.getLogger(__name__)
 
 
-def validate_request_data(config: Config, request_data: Dict) -> None:
+def _validate_message_dict(message_dict: Mapping[str, Any]) -> BasePendingMessage:
+    try:
+        return parse_message(message_dict)
+    except InvalidMessageException as e:
+        raise web.HTTPUnprocessableEntity(body=str(e))
+
+
+def _validate_request_data(config: Config, request_data: Dict) -> None:
     """
     Validates the content of a JSON pubsub message depending on the channel
     and raises a 422 error if the data does not match the expected format.
@@ -30,37 +51,34 @@ def validate_request_data(config: Config, request_data: Dict) -> None:
     # Currently, we only check validate messages
     message_topic = config.aleph.queue_topic.value
     if topic == message_topic:
-        message = json.loads(cast(str, request_data.get("data")))
-        try:
-            _ = parse_message(message)
-        except InvalidMessageException as e:
-            raise web.HTTPUnprocessableEntity(body=str(e))
+        message_dict = json.loads(cast(str, request_data.get("data")))
+        _validate_message_dict(message_dict)
 
 
-async def pub_json(request: web.Request):
-    """Forward the message to P2P host and IPFS server as a pubsub message"""
-    request_data = await request.json()
-    validate_request_data(config=request.app["config"], request_data=request_data)
-
+async def _pub_on_p2p_topics(
+    p2p_client: AlephP2PServiceClient,
+    ipfs_service: Optional[IpfsService],
+    topic: str,
+    payload: Union[str, bytes],
+) -> List[Protocol]:
     failed_publications = []
 
-    try:
-        ipfs_service: IpfsService = request.app["storage_service"].ipfs_service
-        if request.app["config"].ipfs.enabled.value:
+    if ipfs_service:
+        try:
             await asyncio.wait_for(
-                ipfs_service.pub(request_data.get("topic"), request_data.get("data")), 10
+                ipfs_service.pub(topic, payload),
+                10,
             )
-    except Exception:
-        LOGGER.exception("Can't publish on ipfs")
-        failed_publications.append(Protocol.IPFS)
+        except Exception:
+            LOGGER.exception("Can't publish on ipfs")
+            failed_publications.append(Protocol.IPFS)
 
     try:
-        p2p_client: AlephP2PServiceClient = request.app["p2p_client"]
         await asyncio.wait_for(
             pub_p2p(
                 p2p_client,
-                request_data.get("topic"),
-                request_data.get("data"),
+                topic,
+                payload,
                 loopback=True,
             ),
             10,
@@ -69,13 +87,135 @@ async def pub_json(request: web.Request):
         LOGGER.exception("Can't publish on p2p")
         failed_publications.append(Protocol.P2P)
 
+    return failed_publications
+
+
+def _get_request_status(failed_publications: Collection[Protocol]) -> str:
     status = {
         0: "success",
         1: "warning",
         2: "error",
     }[len(failed_publications)]
+    return status
+
+
+async def pub_json(request: web.Request):
+    """Forward the message to P2P host and IPFS server as a pubsub message"""
+    request_data = await request.json()
+    _validate_request_data(
+        config=get_config_from_request(request), request_data=request_data
+    )
+
+    ipfs_service = get_ipfs_service_from_request(request)
+    p2p_client = get_p2p_client_from_request(request)
+
+    failed_publications = await _pub_on_p2p_topics(
+        p2p_client=p2p_client,
+        ipfs_service=ipfs_service,
+        topic=request_data.get("topic"),
+        payload=request_data.get("data"),
+    )
+    status = _get_request_status(failed_publications)
 
     return web.json_response(
         {"status": status, "failed": failed_publications},
         status=500 if status == "error" else 200,
+    )
+
+
+async def _mq_read_one_message(
+    queue: aio_pika.abc.AbstractQueue, timeout: float
+) -> Optional[aio_pika.abc.AbstractIncomingMessage]:
+    """
+    Believe it or not, this is the only way I found to
+    :return:
+    """
+    try:
+        async with queue.iterator(timeout=timeout, no_ack=True) as queue_iter:
+            async for message in queue_iter:
+                return message
+
+    except asyncio.TimeoutError:
+        pass
+
+    return None
+
+
+class PubMessageRequest(BaseModel):
+    sync: bool = False
+    message_dict: Dict[str, Any] = Field(alias="message")
+
+
+async def pub_message(request: web.Request):
+    try:
+        request_data = PubMessageRequest.parse_obj(await request.json())
+    except ValidationError as e:
+        raise web.HTTPUnprocessableEntity(body=e.json(indent=4))
+
+    pending_message = _validate_message_dict(request_data.message_dict)
+
+    # In sync mode, wait for a message processing event. We need to create the queue
+    # before publishing the message on P2P topics in order to guarantee that the event
+    # will be picked up.
+    config = get_config_from_request(request)
+
+    if request_data.sync:
+        mq_conn = get_mq_conn_from_request(request)
+        mq_queue = await mq_make_aleph_message_topic_queue(
+            mq_conn=mq_conn, config=config, routing_key=f"*.{pending_message.item_hash}"
+        )
+    else:
+        mq_queue = None
+
+    # We publish the message on P2P topics early, for 3 reasons:
+    # 1. Just because this node is unable to process the message does not
+    #    necessarily mean the message is incorrect (ex: bug in a new version).
+    # 2. If the publication fails after the processing, we end up in a situation where
+    #    a message exists without being propagated to the other nodes, ultimately
+    #    causing sync issues on the network.
+    # 3. The message is currently fed to this node using the P2P service client
+    #    loopback mechanism.
+    ipfs_service = get_ipfs_service_from_request(request)
+    p2p_client = get_p2p_client_from_request(request)
+
+    message_topic = config.aleph.queue_topic.value
+    failed_publications = await _pub_on_p2p_topics(
+        p2p_client=p2p_client,
+        ipfs_service=ipfs_service,
+        topic=message_topic,
+        payload=aleph_json.dumps(request_data.message_dict),
+    )
+
+    # When publishing in async mode, just respond with 202 (Accepted).
+    message_accepted_response = web.json_response(
+        {"status": MessageStatus.PENDING}, status=202
+    )
+    if not request_data.sync:
+        return message_accepted_response
+
+    # Ignore type checking here, we know that mq_queue is set at this point
+    assert mq_queue is not None
+    response = await _mq_read_one_message(mq_queue, timeout=5)
+
+    # Delete the queue immediately
+    await mq_queue.delete(if_empty=False)
+
+    # If the message was not processed before the timeout, return a 202.
+    if response is None:
+        return message_accepted_response
+
+    routing_key = response.routing_key
+    assert routing_key is not None  # again, for type checking
+    status_str, _item_hash = routing_key.split(".")
+    processing_status = MessageProcessingStatus(status_str)
+
+    if processing_status == MessageProcessingStatus.FAILED_REJECTED:
+        raise web.HTTPUnprocessableEntity(
+            body=f"Failed to process {pending_message.item_hash}"
+        )
+    if processing_status == MessageProcessingStatus.FAILED_WILL_RETRY:
+        return message_accepted_response
+
+    return web.json_response(
+        {"status": processing_status.to_message_status().value}, status=200
     )
